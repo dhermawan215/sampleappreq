857491491e20fb5cf2a3334c07c34878ade4f5dc
dhermawan215 <dhermawan215@gmail.com>
2022-10-05 16:52:52 +0700

    feature login

---
+++ login.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic Page Info -->
+    <meta charset="utf-8" />
+    <title>Sample Request App-Login</title>
+    <?php include('layouts/css.php') ?>
+</head>
+
+<body class="login-page">
+
+    <div class="login-wrap d-flex align-items-center flex-wrap justify-content-center">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 col-lg-7">
+                    <img src="/public/img/3619024.png" alt="" />
+                </div>
+                <!-- <div class="col-md-6 col-lg-7">
+                    <img src="/public/vendors/images/login-page-img.png" alt="" />
+                </div> -->
+                <div class="col-md-6 col-lg-5">
+                    <div class="login-box bg-white box-shadow border-radius-10">
+                        <div class="login-title">
+                            <h2 class="text-center text-primary">Sample Request App</h2>
+                        </div>
+                        <form>
+
+                            <div class="input-group custom">
+                                <input type="text" class="form-control form-control-lg" placeholder="email" />
+                                <div class="input-group-append custom">
+                                    <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+                                </div>
+                            </div>
+                            <div class="input-group custom">
+                                <input type="password" class="form-control form-control-lg" placeholder="**********" />
+                                <div class="input-group-append custom">
+                                    <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
+                                </div>
+                            </div>
+                            <div class="row pb-30">
+                                <div class="col-6">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="customCheck1" />
+                                        <label class="custom-control-label" for="customCheck1">Remember</label>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="forgot-password">
+                                        <a href="forgot-password.html">Forgot Password</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="input-group mb-0">
+                                        <!--
+											use code for form submit
+											<input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
+										-->
+                                        <button class="btn btn-primary btn-lg btn-block" href="index.html">Sign In</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include('layouts/js.php') ?>
+</body>
+
+</html>
